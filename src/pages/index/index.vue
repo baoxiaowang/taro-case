3f2c17839e493fb90b06d51ceefb0d981d21a38b
baoxiaowang <465782548@qq.com>
2024-03-04 16:27:45 +0800

    fix: case

---
+++ src/pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <view class="demo">
+    <view>
+      欢迎使用 NutUI 开发 Taro 多端项目 <Dongdong />
+    </view>
+    <view>{{ count }}</view>
+    <view>
+      <nut-button type="primary" @click="handleClick">
+        Count++
+      </nut-button>
+    </view>
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { Dongdong } from '@nutui/icons-vue-taro';
+import Taro from '@tarojs/taro'
+
+const count = ref(0);
+
+const handleClick = () => {
+  Taro.navigateTo({
+    url: 'pages/test/index'
+  })
+};
+</script>
+
+<style>
+.demo {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
